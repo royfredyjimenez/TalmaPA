62bc78a80dbe1a31b09802f25a0c3326abf089df
royfredyjimenez <royfredyjimenez@users.noreply.github.com>
2017-07-16 17:38:27 -0500

    Ejemplo de carga de un csv de OMA

---
+++ Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtCSV = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'txtCSV
+        '
+        Me.txtCSV.Location = New System.Drawing.Point(29, 12)
+        Me.txtCSV.Multiline = True
+        Me.txtCSV.Name = "txtCSV"
+        Me.txtCSV.Size = New System.Drawing.Size(470, 162)
+        Me.txtCSV.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(531, 186)
+        Me.Controls.Add(Me.txtCSV)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtCSV As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim lines = IO.File.ReadAllLines("F:\cargacsv.csv")
+        Dim dTable = New DataTable
+        Dim numeroColumnas = lines.First.Split(","c).Length
+
+        For i As Int32 = 1 To numeroColumnas
+            'Agrega columnas a DataTable.
+            dTable.Columns.Add(New DataColumn("Columna_" & i, GetType(String)))
+            Debug.Print("Columna_" & i)
+        Next
+
+        '**********************
+        For Each line In lines
+            Dim objFields = From field In line.Split(","c)
+            Dim newRow = dTable.Rows.Add()
+            newRow.ItemArray = objFields.ToArray()
+            txtCSV.Text = objFields.ToArray().GetValue(0).ToString
+            txtCSV.Text = objFields.ToArray().GetValue(1).ToString
+            txtCSV.Text = objFields.ToArray().GetValue(2).ToString
+            'txtCSV.Text = objFields.ToArray().GetValue(1).ToString
+            'txtCSV.Text = objFields.ToArray().GetValue(1).ToString
+
+            'Debug.Print("columna_1: " & objFields.ToArray().GetValue(0).ToString)
+            'Debug.Print("columna_2: " & objFields.ToArray().GetValue(1).ToString)
+            'Debug.Print("columna_3: " & objFields.ToArray().GetValue(2).ToString)
+        Next
+
+        'Debug.Print("finaliza!")
+
+
+    End Sub
+End Class
